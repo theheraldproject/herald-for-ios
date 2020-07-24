5e221b877214eed63582a649cd0d07c7a3702a0d
C19X <support@c19x.org>
2020-07-24 13:18:43 +0100

    Basic framework with persistent logging

---
+++ C19X-SENSOR-iOS/Data/Database.swift
@@ -0,0 +1,103 @@
+//
+//  Database.swift
+//  C19X-SENSOR-iOS
+//
+//  Created by Freddy Choi on 24/07/2020.
+//  Copyright © 2020 C19X. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import os
+
+protocol Database {
+    func insert(_ event: String)
+    func exportEvents()
+}
+
+class MockDatabase: Database {
+    func insert(_ event: String) {
+    }
+    func exportEvents() {
+    }
+}
+
+@available(iOS 10.0, *)
+class ConcreteDatabase: Database {
+    static let shared = ConcreteDatabase()
+    private let logger = ConcreteLogger(subsystem: "Data", category: "Database")
+    private let dispatchQueue = DispatchQueue(label: "Database")
+    private var persistentContainer: NSPersistentContainer
+    private var events: [Event] = []
+
+    init() {
+        persistentContainer = NSPersistentContainer(name: "C19X_SENSOR_iOS")
+        let storeDirectory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+        let url = storeDirectory.appendingPathComponent("C19X_SENSOR_iOS.sqlite")
+        let description = NSPersistentStoreDescription(url: url)
+        description.shouldInferMappingModelAutomatically = true
+        description.shouldMigrateStoreAutomatically = true
+        description.setOption(FileProtectionType.completeUntilFirstUserAuthentication as NSObject, forKey: NSPersistentStoreFileProtectionKey)
+        persistentContainer.persistentStoreDescriptions = [description]
+        persistentContainer.loadPersistentStores { description, error in
+            description.options.forEach() { option in
+                self.logger.log(.debug, "Loaded persistent stores (key=\(option.key),value=\(option.value.description))")
+            }
+            if let error = error {
+                fatalError("Unable to load persistent stores: \(error.localizedDescription)")
+            }
+        }
+        load()
+    }
+    
+    func insert(_ event: String) {
+        do {
+            let managedContext = persistentContainer.viewContext
+            let object = NSEntityDescription.insertNewObject(forEntityName: "Event", into: managedContext) as! Event
+            object.setValue(Date(), forKey: "time")
+            object.setValue(event, forKey: "event")
+            try managedContext.save()
+            events.append(object)
+        } catch {
+            logger.log(.fault, "insert failed (event=\(event),error=\(error.localizedDescription))")
+        }
+    }
+    
+    func exportEvents() {
+        dispatchQueue.async {
+            do {
+                let fileURL = try FileManager.default
+                    .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                    .appendingPathComponent("events.csv")
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                var string = "time,event\n"
+                self.events.forEach() { event in
+                    guard let time = event.time, let eventText = event.event else {
+                        return
+                    }
+                    let timestamp = dateFormatter.string(from: time)
+                    let row = timestamp + ",\"" + eventText + "\"\n"
+                    string.append(row)
+                }
+                print(string)
+                try string.write(to: fileURL, atomically: true, encoding: .utf8)
+                self.logger.log(.debug, "export")
+            } catch {
+                self.logger.log(.fault, "export failed (error=\(error.localizedDescription))")
+            }
+        }
+    }
+
+    
+    private func load() {
+        let managedContext = persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<Event>(entityName: "Event")
+        do {
+            events = try managedContext.fetch(fetchRequest)
+            logger.log(.debug, "Loaded (count=\(events.count))")
+        } catch let error as NSError {
+            logger.log(.fault, "Load failed (error=\(error.description))")
+        }
+    }
+}
---
+++ C19X-SENSOR-iOS/Data/Logger.swift
@@ -0,0 +1,83 @@
+//
+//  Logger.swift
+//  C19X-SENSOR-iOS
+//
+//  Created by Freddy Choi on 24/07/2020.
+//  Copyright © 2020 C19X. All rights reserved.
+//
+
+import Foundation
+import os
+
+protocol Logger {
+    init(subsystem: String, category: String)
+    
+    func log(_ level: LogLevel, _ message: String)
+    
+    func debug(_ message: String)
+    
+    func info(_ message: String)
+    
+    func fault(_ message: String)
+}
+
+enum LogLevel: String {
+    case debug, info, fault
+}
+
+class ConcreteLogger: NSObject, Logger {
+    private let subsystem: String
+    private let category: String
+    private let log: OSLog?
+    private let database: Database?
+    
+    required init(subsystem: String, category: String) {
+        self.subsystem = subsystem
+        self.category = category
+        if #available(iOS 10.0, *) {
+            log = OSLog(subsystem: subsystem, category: category)
+            database = ConcreteDatabase.shared
+        } else {
+            log = nil
+            database = nil
+        }
+    }
+
+    func log(_ level: LogLevel, _ message: String) {
+        // Write to unified os log if available, else print to console
+        guard let log = log else {
+            let entry = Date().description + "::" + level.rawValue + "::" + subsystem + "::" + category + " :: " + message
+            debugPrint(entry)
+            return
+        }
+        if #available(iOS 10.0, *) {
+            switch (level) {
+            case .debug:
+                os_log("%s", log: log, type: .debug, message)
+            case .info:
+                os_log("%s", log: log, type: .info, message)
+            case .fault:
+                os_log("%s", log: log, type: .fault, message)
+            }
+            return
+        }
+        // Write to database for post event analysis
+        guard let database = database else {
+            return
+        }
+        database.insert(level.rawValue + "::" + subsystem + "::" + category + " :: " + message)
+    }
+    
+    func debug(_ message: String) {
+        log(.debug, message)
+    }
+    
+    func info(_ message: String) {
+           log(.debug, message)
+       }
+    
+    func fault(_ message: String) {
+           log(.debug, message)
+       }
+
+}
