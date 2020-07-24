68361888835d00f09a54ee7727bdbc6884e70a96
C19X <support@c19x.org>
2020-07-24 09:59:39 +0100

    Initial Commit

---
+++ C19X-SENSOR-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C19X-SENSOR-iOS
+//
+//  Created by Freddy Choi on 24/07/2020.
+//  Copyright © 2020 C19X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
