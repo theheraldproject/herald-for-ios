4aa3aabfd32d8cbd2a99513207557037c6c6c5a4
C19X <support@c19x.org>
2020-07-25 09:41:01 +0100

    Prepare for integration with C19X beacon

--- C19X-SENSOR-iOS/Sensor/BLE/BLESensor.swift
+++ C19X-SENSOR-iOS/Sensor/BLE/BLESensor.swift
@@ -7,9 +7,23 @@
 //
 
 import Foundation
+import CoreBluetooth
 
 protocol BLESensor : Sensor {
 }
+
+/// Defines BLE sensor configuration data, e.g. service and characteristic UUIDs
+struct BLESensorConfiguration {
+    /// BLE beacon service
+    static let serviceUUID = CBUUID(string: "0022D481-83FE-1F13-0000-000000000000")
+    /// Signaling characteristic for controlling connection between peripheral and central, e.g. keep each other from suspend state
+    static let signalCharacteristicUUID = CBUUID(string: "0022D481-83FE-1F13-0000-000000000001")
+    /// Primary payload characteristic (read) for distributing payload data from peripheral to central, e.g. identity data
+    static let payloadCharacteristicUUID = CBUUID(string: "0022D481-83FE-1F13-0000-000000000002")
+    /// Secondary payload characteristic (read) for sharing payload data acquired by this central, e.g. identity data of other peripherals in the vincinity
+    static let sharedPayloadCharacteristicUUID = CBUUID(string: "0022D481-83FE-1F13-0000-000000000003")
+}
+
 
 /**
 BLE sensor based on CoreBluetooth
