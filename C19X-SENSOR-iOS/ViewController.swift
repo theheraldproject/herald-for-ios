9645975f4140817b57108c36ed82a13909792654
C19X <support@c19x.org>
2020-08-21 11:51:17 +0100

    Preparing to change connection priority to fit with Android A10 and A20

--- C19X-SENSOR-iOS/ViewController.swift
+++ C19X-SENSOR-iOS/ViewController.swift
@@ -20,8 +20,9 @@ class ViewController: UIViewController, SensorDelegate {
     private var didMeasure = 0
     private var didShare = 0
     private var didVisit = 0
-    private var didReadPayloads: Set<String> = []
-    private var didSharePayloads: Set<String> = []
+    private var payloads: [TargetIdentifier:String] = [:]
+    private var didReadPayloads: [String:Date] = [:]
+    private var didSharePayloads: [String:Date] = [:]
 
     @IBOutlet weak var labelDevice: UILabel!
     @IBOutlet weak var labelPayload: UILabel!
@@ -80,28 +81,31 @@ class ViewController: UIViewController, SensorDelegate {
     }
     
     private func updateDetection() {
-        var payloadPrefixes: [String:String] = [:]
-        didReadPayloads.forEach() { payload in
-            let payloadPrefix = String(payload.prefix(payloadPrefixLength))
-            payloadPrefixes[payloadPrefix] = "read"
+        var payloadShortNames: [String:String] = [:]
+        var payloadLastSeenDates: [String:Date] = [:]
+        didReadPayloads.forEach() { payloadShortName, date in
+            payloadShortNames[payloadShortName] = "read"
+            payloadLastSeenDates[payloadShortName] = didReadPayloads[payloadShortName]
         }
-        didSharePayloads.forEach() { payload in
-            let payloadPrefix = String(payload.prefix(payloadPrefixLength))
-            if payloadPrefixes[payloadPrefix] == nil {
-                payloadPrefixes[payloadPrefix] = "shared"
+        didSharePayloads.forEach() { payloadShortName, date in
+            if payloadShortNames[payloadShortName] == nil {
+                payloadShortNames[payloadShortName] = "shared"
             } else {
-                payloadPrefixes[payloadPrefix] = "read,shared"
+                payloadShortNames[payloadShortName] = "read,shared"
+            }
+            if let didSharePayloadDate = didSharePayloads[payloadShortName], let didReadPayloadDate = didReadPayloads[payloadShortName], didSharePayloadDate > didReadPayloadDate {
+                payloadLastSeenDates[payloadShortName] = didSharePayloadDate
             }
         }
-        var payloadPrefixesList: [String] = []
-        payloadPrefixes.keys.forEach() { payloadPrefix in
-            if let method = payloadPrefixes[payloadPrefix] {
-                payloadPrefixesList.append("\(payloadPrefix) (\(method))")
+        var payloadShortNameList: [String] = []
+        payloadShortNames.keys.forEach() { payloadShortName in
+            if let method = payloadShortNames[payloadShortName], let lastSeenDate = payloadLastSeenDates[payloadShortName] {
+                payloadShortNameList.append("\(payloadShortName) [\(method)] (\(dateFormatter.string(from: lastSeenDate)))")
             }
         }
-        payloadPrefixesList.sort()
-        textViewPayloads.text = payloadPrefixesList.joined(separator: "\n")
-        labelDetection.text = "DETECTION (\(payloadPrefixesList.count))"
+        payloadShortNameList.sort()
+        textViewPayloads.text = payloadShortNameList.joined(separator: "\n")
+        labelDetection.text = "DETECTION (\(payloadShortNameList.count))"
     }
 
     // MARK:- SensorDelegate
@@ -115,7 +119,8 @@ class ViewController: UIViewController, SensorDelegate {
 
     func sensor(_ sensor: SensorType, didRead: PayloadData, fromTarget: TargetIdentifier) {
         self.didRead += 1
-        didReadPayloads.insert(didRead.shortName)
+        payloads[fromTarget] = didRead.shortName
+        didReadPayloads[didRead.shortName] = Date()
         DispatchQueue.main.async {
             self.labelDidRead.text = "didRead: \(self.didRead) (\(self.timestamp()))"
             self.updateDetection()
@@ -124,7 +129,8 @@ class ViewController: UIViewController, SensorDelegate {
 
     func sensor(_ sensor: SensorType, didShare: [PayloadData], fromTarget: TargetIdentifier) {
         self.didShare += 1
-        didShare.forEach { self.didSharePayloads.insert($0.shortName) }
+        let time = Date()
+        didShare.forEach { self.didSharePayloads[$0.shortName] = time }
         DispatchQueue.main.async {
             self.labelDidShare.text = "didShare: \(self.didShare) (\(self.timestamp()))"
             self.updateDetection()
@@ -133,6 +139,9 @@ class ViewController: UIViewController, SensorDelegate {
 
     func sensor(_ sensor: SensorType, didMeasure: Proximity, fromTarget: TargetIdentifier) {
         self.didMeasure += 1;
+        if let payloadShortName = payloads[fromTarget] {
+            didReadPayloads[payloadShortName] = Date()
+        }
         DispatchQueue.main.async {
             self.labelDidMeasure.text = "didMeasure: \(self.didMeasure) (\(self.timestamp()))"
         }
